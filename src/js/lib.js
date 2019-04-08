a87d7f89793f211c924985d3b936cdc76ae3d608
Maxim Usanov <max.usanov@gmail.com>
2019-04-08 23:55:49 +0300

    add func

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,3 +1,11 @@
-export function maximumSalesCalc(sales) {
+export function maximumSalesCalc(items) {
+    let percent = 0.05;
+    let total = 0;
 
+    for (let item of items) {
+        if (item > 10000) {
+            total = total + item;
+        }
+    }
+    return total * percent;
 }
